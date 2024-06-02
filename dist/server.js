5c7beedee3c1c82fcddd4cadb8dc6db4f1c25c47
Manthan <manthanbhatia367@gmail.com>
2024-06-02 10:27:26 +0530

    add new file

--- dist/server.js
+++ dist/server.js
@@ -8,7 +8,7 @@ const mongoose_1 = __importDefault(require("mongoose"));
 const controller_1 = require("./controller/controller");
 const app = (0, express_1.default)();
 const port = process.env.PORT || 3000;
-const mongoUri = "mongodb://localhost:27017/";
+const mongoUri = process.env.MONGO_URI || "mongodb://localhost:27017/";
 app.use(express_1.default.json());
 app.post("/signup", controller_1.signUp);
 app.post("/signin", controller_1.signIn);
